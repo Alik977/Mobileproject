74d84940497a26fe243df53dd1b8dfd1676aca50
Alik977 <alik.hakobyan.sh@gmail.com>
2025-11-05 19:38:40 +0400

    finishing

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,9 @@ import slide1 from "..//../assets/Image/slide1.jpg";
 import slide2 from "..//../assets/Image/slide2.jpg";
 import slide3 from "..//../assets/Image/slide3.jpg";
 import slide4 from "..//../assets/Image/slide4.jpg";
+import slide5 from "..//../assets/Image/slide5.jpg";
+import slide6 from "..//../assets/Image/slide6.jpg";
+import slide7 from "..//../assets/Image/slide7.jpg";
 const Home = () => {
   const settings = {
     dots: true,
@@ -32,6 +35,15 @@ const Home = () => {
   </div>
   <div>
     <img src={slide4} alt="Slide 4" />
+  </div>
+    <div>
+    <img src={slide5} alt="Slide 5" />
+  </div>
+    <div>
+    <img src={slide6} alt="Slide 6" />
+  </div>
+    <div>
+    <img src={slide7} alt="Slide 7" />
   </div>
 </Slider>
 </div>
